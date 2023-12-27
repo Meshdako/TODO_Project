31215644475a8c97dfa7b74e0377e4bc1941b303
Meshdako <msobarzo@utem.cl>
2023-12-26 23:21:10 -0300

    UPDATE: Clase TodoList actualizada para almacenar la información de título, descripción, fechas y un vector de tareas.

--- include/TodoList.h
+++ include/TodoList.h
@@ -5,4 +5,47 @@
 #ifndef TODO_PROJECT_TODOLIST_H
 #define TODO_PROJECT_TODOLIST_H
 
+#include <iostream>
+#include <vector>
+
+#include "Task.h"
+
+class TodoList {
+    private:
+        int id;
+        std::string title;
+        std::string description;
+        std::string status;
+        std::string dueDate;
+        std::string createdAt;
+        std::string updatedAt;
+        std::vector<Task> tasks;
+
+    public:
+        TodoList(int id, std::string title, std::string description, std::string status, std::string dueDate, std::string createdAt, std::string updatedAt);
+        
+        // Getters
+        int getId() { return id; }
+        std::string getTitle() { return title; }
+        std::string getDescription() { return description; }
+        std::string getStatus() { return status; }
+        std::string getDueDate() { return dueDate; }
+        std::string getCreatedAt() { return createdAt; }
+        std::string getUpdatedAt() { return updatedAt; }
+        std::vector<Task>& getTasks() { return tasks; }
+
+        // Setters
+        void setId(int id) { this->id = id; }
+        void setTitle(std::string title) { this->title = title; }
+        void setDescription(std::string description) { this->description = description;}
+        void setStatus(std::string status) { this->status = status; }
+        void setDueDate(std::string dueDate) { this->dueDate = dueDate; }
+        void setCreatedAt(std::string createdAt) { this->createdAt = createdAt; }
+        void setUpdatedAt(std::string updatedAt) { this->updatedAt = updatedAt; }
+
+        // Methods
+        void print();
+        void addTask(const std::string&, const std::string&, const std::string&);
+};
+
 #endif //TODO_PROJECT_TODOLIST_H
--- src/TodoList.cpp
+++ src/TodoList.cpp
@@ -1,3 +1,33 @@
 //
 // Created by meshdako on 20-11-23.
 //
+
+#include "../include/TodoList.h"
+
+TodoList::TodoList(int id, std::string title, std::string description, std::string status, std::string dueDate, std::string createdAt, std::string updatedAt) {
+    this->id = id;
+    this->title = title;
+    this->description = description;
+    this->status = status;
+    this->dueDate = dueDate;
+    this->createdAt = createdAt;
+    this->updatedAt = updatedAt;
+}
+
+void TodoList::print() {
+    std::cout << "ID: " << id << "\n"
+            << "Title: " << title << "\n"
+            << "Description: " << description << "\n"
+            << "Status: " << status << "\n"
+            << "Due Date: " << dueDate << "\n"
+            << "Created At: " << createdAt << "\n"
+            << "Updated At: " << updatedAt << "\n";
+    std::cout << "Tasks: \n";
+    for (const auto& task : tasks) {
+        task.print();
+    }
+}
+
+void TodoList::addTask(const std::string& title, const std::string& desc, const std::string& createdAt) {
+    tasks.push_back(Task(title, desc, createdAt));
+}
