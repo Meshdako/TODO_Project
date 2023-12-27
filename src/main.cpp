1ca5d362b65929e14bb31da3e84487246a39cb62
Meshdako <msobarzo@utem.cl>
2023-12-26 23:21:41 -0300

    UPDATE: Archivo principal con las pruebas

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
+// Task
+#include "../include/Task.h"
+
+// TodoList
+#include "../include/TodoList.h"
+
+#include "../include/Date.h"
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    TodoList myList(1, "Mi lista", "Esta es mi lista de tareas", "pendiente", "2022-12-31", "2022-01-01", "2022-01-01");
+    myList.addTask("Mi tarea", "Esta es mi tarea", getCurrentDate());
+
+    myList.print();
     return 0;
 }
