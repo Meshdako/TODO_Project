b4d93930459f3a5c2847720c58c5097b87f8d131
Meshdako <msobarzo@utem.cl>
2023-12-26 23:20:21 -0300

    UPDATE: Estructura Task actualizada para guardar un nombre, la descripción, la fecha en la que se creó y mantener si es que este a sido completado o no

--- include/Task.h
+++ include/Task.h
@@ -5,4 +5,17 @@
 #ifndef TODO_PROJECT_TASK_H
 #define TODO_PROJECT_TASK_H
 
+#include <iostream>
+
+struct Task {
+    std::string _title;
+    std::string _description;
+    std::string _createdAt;
+    bool _completed;
+
+    Task(const std::string& title, const std::string& desc, const std::string& createdAt);
+    void print() const;
+};
+
+
 #endif //TODO_PROJECT_TASK_H
--- src/Task.cpp
+++ src/Task.cpp
@@ -1,3 +1,15 @@
 //
 // Created by meshdako on 20-11-23.
 //
+
+#include "../include/Task.h"
+
+Task::Task(const std::string& title, const std::string& desc, const std::string& createdAt) 
+    : _title(title), _description(desc), _createdAt(createdAt), _completed(false) {}
+
+void Task::print() const {
+    std::cout << "Title: " << _title << "\n"
+                << "Description: " << _description << "\n"
+                << "Created At: " << _createdAt << "\n"
+                << "Completed: " << (_completed ? "Yes" : "No") << "\n";
+}
